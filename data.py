b599aecf3a8c17d36b50377fce7f204cf8c39707
Akshit Gupta <akshitgupta1996@gmail.com>
2019-03-13 03:02:01 -0700

    created articles page

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+  articles = [
+    {
+      'id': 1,
+      'title': 'Article One',
+      'body': 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et assumenda aut veniam aliquid consequuntur eos harum officia in. Animi repellendus sapiente laudantium tempora, explicabo cupiditate est nesciunt a consequatur ullam?',
+      'author': 'Akshit Gupta',
+      'create_date': '04-25-2018'
+    },
+    {
+      'id': 2,
+      'title': 'Article Two',
+      'body': 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et assumenda aut veniam aliquid consequuntur eos harum officia in. Animi repellendus sapiente laudantium tempora, explicabo cupiditate est nesciunt a consequatur ullam?',
+      'author': 'John Doe',
+      'create_date': '04-25-2018'
+    },
+    {
+      'id': 3,
+      'title': 'Article Three',
+      'body': 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et assumenda aut veniam aliquid consequuntur eos harum officia in. Animi repellendus sapiente laudantium tempora, explicabo cupiditate est nesciunt a consequatur ullam?',
+      'author': 'John Doe',
+      'create_date': '04-25-2018'
+    },
+  ]
+  return articles
