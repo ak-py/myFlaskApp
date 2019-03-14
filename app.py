c4abb7b5c2c6a9442f65883a3fc00a148f5b1748
Akshit Gupta <akshitgupta1996@gmail.com>
2019-03-14 05:02:45 -0700

    installing and updating packages for SQL and auth

--- app.py
+++ app.py
@@ -1,25 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 from data import Articles
+from flaskext.mysql import MYSQL
+from wtform import Form, StringField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 
 Articles = Articles()
 
+
 @app.route('/')
 def index():
-  return render_template('home.html')
+    return render_template('home.html')
+
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
+
 
 @app.route('/articles')
 def articles():
-  return render_template('articles.html', articles=Articles)
+    return render_template('articles.html', articles=Articles)
+
 
 @app.route('/article/<string:id>/')
 def article(id):
-  return render_template('article.html', id=id)
+    return render_template('article.html', id=id)
+
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        pass
+
+    return render_template('register.html', form=form)
+
 
 if(__name__ == '__main__'):
-  app.run(debug=True)
+    app.run(debug=True)
